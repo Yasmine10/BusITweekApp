40abd0e7ff9ae071a44734a15c6035b57b5bff43
Petr Tykal <tykal.pete@gmail.com>
2018-02-07 19:45:23 +0100

    Working on tutorial

--- app/src/main/java/cz/pef/mendelu/busitweek5/activity/TutorialActivity.java
+++ app/src/main/java/cz/pef/mendelu/busitweek5/activity/TutorialActivity.java
@@ -1,20 +1,49 @@
 package cz.pef.mendelu.busitweek5.activity;
 
 import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import cz.pef.mendelu.busitweek5.R;
+import cz.pef.mendelu.busitweek5.adapters.TutorialAdapter;
 
 public class TutorialActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private TutorialAdapter tutorialAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tutorial);
 
+        initViews();
+        setUpViewPager();
         //FOR TUTORIAL
+    }
+
+    /**
+     * Initialize views
+     */
+    private void initViews() {
+        viewPager = findViewById(R.id.tutorial_view_pager);
+    }
+
+    /**
+     * Set up ViewPager for tutorial
+     */
+    private void setUpViewPager() {
+        List<Integer> pages = new ArrayList<>();
+        pages.add(R.layout.tutorial_first);
+        pages.add(R.layout.tutorial_second);
+        tutorialAdapter = new TutorialAdapter(pages);
+        viewPager.setAdapter(tutorialAdapter);
+        //tabLayout.setupWithViewPager(viewPager, true);
     }
 
     //temporary button for opening map
---
+++ app/src/main/java/cz/pef/mendelu/busitweek5/adapters/TutorialAdapter.java
@@ -0,0 +1,41 @@
+package cz.pef.mendelu.busitweek5.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+public class TutorialAdapter extends PagerAdapter {
+
+    private List<Integer> pages;
+
+    public TutorialAdapter(List<Integer> pages) {
+        this.pages = pages;
+    }
+
+    @Override
+    public int getCount() {
+        return pages.size();
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        LayoutInflater inflater = (LayoutInflater) container.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        int resLayoutId = pages.get(position);
+
+        View view = inflater.inflate(resLayoutId, null);
+        container.addView(view, 0);
+        return view;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+}
