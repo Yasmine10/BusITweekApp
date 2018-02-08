e84b8be04945feef4518a8d0d85b78d1607ce2f5
Petr Tykal <tykal.pete@gmail.com>
2018-02-08 14:39:30 +0100

    Working on the grid

--- app/src/main/java/cz/pef/mendelu/busitweek5/activity/PuzzleImageActivity.java
+++ app/src/main/java/cz/pef/mendelu/busitweek5/activity/PuzzleImageActivity.java
@@ -2,14 +2,32 @@ package cz.pef.mendelu.busitweek5.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 import cz.pef.mendelu.busitweek5.R;
+import cz.pef.mendelu.busitweek5.adapters.GridAdapter;
 
 public class PuzzleImageActivity extends AppCompatActivity {
+
+    private GridView gridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle_image);
+
+        gridView = findViewById(R.id.gridView);
+
+        gridView.setAdapter(new GridAdapter(this, new int[]{
+                R.drawable.top_left,
+                R.drawable.top_center,
+                R.drawable.top_right,
+                R.drawable.center_left,
+                R.drawable.center_part,
+                R.drawable.center_right,
+                R.drawable.given_part,
+                R.drawable.bottom_center,
+                R.drawable.bottom_right
+        }));
     }
 }
